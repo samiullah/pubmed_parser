389db436e222ee8f0cc3b587924f5bf6be02432e
srijan <srijan@srijans-MacBook-Air-3.local>
2018-12-16 10:01:09 +0530

    few changes

--- xmltoexcel.py
+++ xmltoexcel.py
@@ -8,6 +8,8 @@ import sys
 reload(sys)
 from lxml import etree as ET
 from lxml import html
+import ast
+
 try:
     from itertools import zip_longest as zip_longest
 except:
@@ -31,56 +33,83 @@ print('***** Parsing Begins now *******')
 tree = ET.parse(xml_file)
 root = tree.getroot()
 
+
+
+
 #name csv file as searchterm+timestamp
 
 
 def parse_authors(tree):
+    # global affiliations_text
     dictList = []
     affiliations = list()
-    if tree.xpath('//AffiliationInfo/Affiliation') is not None:
-        for affil in tree.xpath('//AffiliationInfo/Affiliation'):
-            affiliations.append(affil.text)
-            affiliations_text = '; '.join(affiliations)
+    naam = list()
+
 
     authors_tree = tree.xpath('//AuthorList/Author')
     authors = list()
-    if authors_tree is not None:
-        for a in authors_tree:
-            firstname = a.find('ForeName').text if a.find('ForeName') is not None else ''
-            lastname = a.find('LastName').text if a.find('LastName') is not None else ''
-            fullname = (firstname + ' ' + lastname).strip()
-            if fullname == '':
-                fullname = a.find('CollectiveName').text if a.find('CollectiveName') is not None else ''
-            authors.append(fullname)
-        authors_text = '; '.join(authors)
-    else:
-        authors_text = ''
+        
+    for each in root.findall('.//AuthorList/Author'):
+        aff = each.find('.//AffiliationInfo/Affiliation');
+        auth = each.find('.//ForeName');
+
+        if aff is not None and auth is not None:
+           
+            lst = re.findall('\S+@\S+',aff.text)
+   
+            affiliations.append(lst)
+            naam.append(auth.text)
+            values = ",".join(map(str, affiliations))
+            affiliations_text = values
+
+    
+
+    
+    print(affiliations)
+
+       
+
+    m=ast.literal_eval(affiliations_text)
+   
+    # print(authors_text)
+
+    for item in m:
+        if len(item) == 0:
+            print('No Email')
+        else:
+            print(item)
 
     dict_out = {
-    'authors': authors_text,
-    'affiliation': affiliations_text}
+    'authors': naam
+    #'affiliation': m
+    }
 
-    x= dict_out['authors'].split(';')
-    print (x)
-    y = dict_out['affiliation'].split(';')
+    x= dict_out['authors']
 
-    newl = list()
-    print(type(newl))
-    pattern = r"[\w.]+@[\w.]+"
-    for item in y:
-        if re.findall(pattern,item):
-            newl.append(item)
-        else:
-            newl.append("noemail@mailinator.com")
+    # we need to remove noemail keep null
+    # only email address
+    # we need to remove duplicate email addresses
+    # if record>9000  =>add custom range
+    # make record <9000 for custom range
+    # Rename files
+    # count xml files 
+    # append all data to csv to only one files
+    # csv filename shd have date range
+    # dnt allow duplicate data
 
-    d=[x,newl]
+    #write code for sending emails based on number of emails based on number
+
+
+
+    d=[x,m]
     export_data = zip_longest(*d, fillvalue = '')
     file = 'numbers.csv'
     with open(file, 'w', encoding="utf8", newline='') as myfile:
         wr = csv.writer(myfile)
-        wr.writerow(("Author", "Email"))
+        wr.writerow(("Author","Email Address"))
         wr.writerows(export_data)
     myfile.close()
+
 
 
   
@@ -88,29 +117,3 @@ def parse_authors(tree):
 parse_authors(tree)
 
 print("***** Parsing Complete *****")
-
-
-
-
-
-
-
-# re_pattern = re.compile(r'[\w\.-]+@[\w\.-]+')
-
-# with open("test.csv") as fh_in:    
-#     with open("mailout.csv", "a+") as fh_out:
-#         for line in fh_in:
-#             match_list = re_pattern.findall(line)
-#             if match_list:
-#                 fh_out.write(match_list[0]+"\r\n") 
-
-# #count the number of emails scrapped
-# reader=csv.reader(open("mailout.csv"))
-# count=0
-# for row in reader:
-#     count+=1
-#     print "total no in row "+str(count)+" is "+str(len(row))
-#     for i in row:
-#         print (i)
-
-
